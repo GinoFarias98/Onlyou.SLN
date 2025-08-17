4e38e87228319987240cd44b6b23620caab20b3d
Lourdes Giorda <lourdesgiorda@outlook.es>
2025-08-17 15:43:33 -0300

    Revert "Modificacion IRepositorioPedido"
    This reverts commit 7148421510442298c20c562594476b85739279a8.

--- Onlyou.Server/Repositorio/IRepositorioPedido.cs
+++ Onlyou.Server/Repositorio/IRepositorioPedido.cs
@@ -6,8 +6,8 @@ namespace Onlyou.Server.Repositorio
     {
         Task<Pedido?> SelectPedidoPorIdAsync(int pedidoId);
         Task<IEnumerable<Pedido>> SelectPedidosPorClienteAsync(string nombreCliente, int? dni = null);
-        //Task<IEnumerable<Pedido>> SelectPedidosPorEstadoEntregaAsync(Pedido.EstadoEntrega estadoEntrega);
-        //Task<IEnumerable<Pedido>> SelectPedidosPorEstadoPagoAsync(Pedido.EstadoPago estadoPago);
+        Task<IEnumerable<Pedido>> SelectPedidosPorEstadoEntregaAsync(Pedido.EstadoEntrega estadoEntrega);
+        Task<IEnumerable<Pedido>> SelectPedidosPorEstadoPagoAsync(Pedido.EstadoPago estadoPago);
         Task<IEnumerable<Pedido>> SelectPedidosPorRangoFechasAsync(DateTime inicio, DateTime fin);
         Task<decimal> SelectSaldoPendienteTotalAsync();
     }
