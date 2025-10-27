6720bca74d1d68ae1cf8ce4d033ad1a35a191941
Gino Farias <fariasgino980@gmail.com>
2025-10-27 09:22:12 -0300

    style(frontend): ajustes en el diseño y estructura visual del modal

--- Onlyou.Client/Helpers/SelectorMultipleModel.cs
+++ Onlyou.Client/Helpers/SelectorMultipleModel.cs
@@ -2,13 +2,15 @@
 {
     public class SelectorMultipleModel
     {
-        public SelectorMultipleModel(string llave, string valor)
+        public SelectorMultipleModel(string llave, string valor , string? hex = null)
         {
             Llave = llave;
             Valor = valor;
+            Hexadecimal = hex;
         }
 
         public string Llave { get; set; }
         public string Valor { get; set; }
+        public string? Hexadecimal { get; set; } // solo para colores
     }
 }
